b42764a1fce63fe098b5f2b9999c57712d27b3c8
ratacat <neuralsplash@gmail.com>
2013-04-23 12:25:40 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Villagecraft::Application.config.secret_token = 'e70624c85799c50d03e88e24b9aeaf173cf1a0e5ec3c9af237e0326a97977d3ae313d12bc3311ff3b8d60c102ab697208df22b4741745f7673cd490ac828195a'
