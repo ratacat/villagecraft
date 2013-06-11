f0b3fa090904d56d67c012898bff76e69618144b
Ben Teitelbaum <ben@teitelbaum.us>
2013-06-11 12:58:47 -0700

    if missing, default title of course to its vclass's title

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,8 +1,16 @@
 class Course < ActiveRecord::Base
+  attr_accessible :title, :vclass
+
   belongs_to :vclass
   has_many :events
-  # attr_accessible :title, :body
   
   validates :vclass_id, presence: true
   validates :title, presence: true
+  
+  before_validation :set_default_values, :on => :create
+  
+  protected
+  def set_default_values
+    self.title ||= self.vclass.try(:title)
+  end
 end
--- app/models/vclass.rb
+++ app/models/vclass.rb
@@ -1,8 +1,9 @@
 class Vclass < ActiveRecord::Base
+  attr_accessible :title, :admin
+
   belongs_to :admin, :class_name => 'User'
   has_many :reviews
   has_many :courses
-  # attr_accessible :title, :body
   
   validates :admin_id, presence: true
   validates :title, presence: true
