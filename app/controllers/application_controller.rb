41e76fd5d9a3f7f3f845919d6d042c22c350c51a
Ben Teitelbaum <ben@teitelbaum.us>
2013-06-13 15:09:24 -0700

    generic error renderer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  def render_error(options={})
+    respond_to do |format|
+      format.html { redirect_to root_path, :alert => options[:message] }
+      format.json { render :json => { :message => options[:message] }, :status => options[:status] }
+    end
+  end
+  
 end
