8276aea743ab21ff1d02dcbe93ad920ee5a6bcfb
Ben Teitelbaum <ben@teitelbaum.us>
2013-06-13 15:09:00 -0700

    use both user edit and user_registration edit views

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :name
-  # attr_accessible :title, :body
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name_first, :name_last
 
   has_many :hostings, :class_name => 'Event', :foreign_key => :host_id
   has_and_belongs_to_many :attends, :class_name => 'Event'
