03e164d7e64863244c0b4a7026e5df5ad4666a12
Ben Teitelbaum <ben@teitelbaum.us>
2013-06-14 02:30:22 -0700

    allow events to be visible to crawlers and other unregistered users

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
   before_filter :find_event, :except => [:index, :my_events, :new, :create]
-  before_filter :authenticate_user!, except: [:index]
+  before_filter :authenticate_user!, except: [:index, :show]
   #before_filter :checkDate, :only => [:create, :update]
   # GET /events
   # GET /events.json
