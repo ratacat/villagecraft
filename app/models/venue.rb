0323804dd7a9f27b943e4c80b21d8b45db7b88c3
Ben Teitelbaum <ben@teitelbaum.us>
2013-06-24 23:56:29 -0700

    prepare the Venue model's association with Location, as was done for the User model

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,7 +1,33 @@
 class Venue < ActiveRecord::Base
-  attr_accessible :name, :location, :owner
+  attr_accessible :name, :street, :city, :state
+  attr_writer :street, :city, :state
 
   belongs_to :owner, :class_name => 'User'
   belongs_to :location
   has_many :events
+  
+  before_validation :find_or_create_location_from_address
+  
+  validates :name, :presence => true
+  validates :owner, :presence => true
+  validates :location, :presence => true
+  validates_associated :location
+
+  def street
+    @street || self.location.try(:street)
+  end
+  
+  def city
+    @city || self.location.try(:city)
+  end
+
+  def state
+    @state || self.location.try(:state_code)
+  end
+  
+  protected
+  def find_or_create_location_from_address
+    self.location = Location.find_or_create_by_street_and_city_and_state_code(:street => self.street, :city => self.city, :state_code => self.state)
+  end
+  
 end
