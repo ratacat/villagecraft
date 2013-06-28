ea64de11a36ba6f5da34f73c26ad2ba52c8a36bc
Ben Teitelbaum <ben@teitelbaum.us>
2013-06-28 08:57:38 -0700

    validations and attribute ACL

--- app/models/events_user.rb
+++ app/models/events_user.rb
@@ -1,4 +1,10 @@
 class EventsUser < ActiveRecord::Base
+  attr_accessible :user, :event, :hosting, :as => :system
+  
   belongs_to :user
   belongs_to :event
+  
+  validates :user, presence: true
+  validates :event, presence: true
+  
 end
