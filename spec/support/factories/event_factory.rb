e95bd9813037f6d65fd2a02c7a387b4b942bb0f4
Ben Teitelbaum <ben@teitelbaum.us>
2013-07-01 12:52:49 -0700

    default short_title to first two words of title

--- spec/support/factories/event_factory.rb
+++ spec/support/factories/event_factory.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :event do
-    title "My new Event"
-    short_title "event"
+    title "Test Event"
+    short_title { title.split[0,2].join(' ') }
     description "this is an event for testing purposes or porpoises"
     start_time { Timeliness.parse("#{(rand(60) + 1).days.from_now.to_date} #{(7..21).to_a.sample}:00") }
     end_time { start_time + [30, 60, 90, 120].sample.minutes }
