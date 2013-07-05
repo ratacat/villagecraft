72de4ffcb210987cb3ce6cc97f2ffc9d15f2c605
Ben Teitelbaum <ben@teitelbaum.us>
2013-07-05 15:13:06 -0700

    move date time picker initialization out of util.js

--- app/assets/javascripts/util.js
+++ app/assets/javascripts/util.js
@@ -1,27 +1,6 @@
 // util.js
 // common scripts
 
-//datetime picker on events creation page
 jQuery(function($){
-
-  $('.date-select-component').datetimepicker({
-    language: 'en',
-    pickTime: false,
-    startDate: '0d'
-  });
-
-  $('.bootstrap-timepicker input').timepicker();
-  
   $(".tooltipify").tooltip();
-  
-  /* FIXME: Incomplete B-list code to constrain a pair of timepickers
-  $("input.constrain-other-datetime[data-constrain-start-datetime-id]").timepicker().on('changeTime.timepicker', function(e) {
-    var start_id = $(this).data('constrain-start-datetime-id'),
-        end_id = $(this).data('constrain-end-datetime-id'),
-        t = e.time.value;
-    if (start_id) {
-      $('#' + start_id).timepicker('setTime', t);
-    }
-  });
-  */
 });
---
+++ app/views/events/_init_datetime_pickers.js
@@ -0,0 +1,18 @@
+$('.date-select-component').datetimepicker({
+  language: 'en',
+  pickTime: false,
+  startDate: '0d'
+});
+
+$('.bootstrap-timepicker input').timepicker();
+
+/* FIXME: Incomplete B-list code to constrain a pair of timepickers
+$("input.constrain-other-datetime[data-constrain-start-datetime-id]").timepicker().on('changeTime.timepicker', function(e) {
+  var start_id = $(this).data('constrain-start-datetime-id'),
+      end_id = $(this).data('constrain-end-datetime-id'),
+      t = e.time.value;
+  if (start_id) {
+    $('#' + start_id).timepicker('setTime', t);
+  }
+});
+*/
