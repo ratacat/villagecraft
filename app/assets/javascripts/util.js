4baa5cba37807cc352e59ede10feba28b60237e7
Ben Teitelbaum <ben@teitelbaum.us>
2013-07-05 15:13:39 -0700

    define String.trim for older versions of Javascript

--- app/assets/javascripts/util.js
+++ app/assets/javascripts/util.js
@@ -1,6 +1,12 @@
 // util.js
 // common scripts
 
+if(!String.prototype.trim) {
+  String.prototype.trim = function () {
+    return this.replace(/^\s+|\s+$/g,'');
+  };
+}
+
 jQuery(function($){
   $(".tooltipify").tooltip();
 });
