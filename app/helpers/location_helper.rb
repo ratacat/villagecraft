d229c0e97695d70c7f4b14b0b3e1eed625eb7fb3
Ben Teitelbaum <ben@teitelbaum.us>
2013-07-06 22:35:10 -0700

    Google Maps goodness

--- app/helpers/location_helper.rb
+++ app/helpers/location_helper.rb
@@ -2,4 +2,13 @@ module LocationHelper
   def link_to_google_maps(location, text=nil)
     link_to(text || location.address, "http://maps.google.com/?#{location.address.to_query('q')}")
   end
+  
+  def map_image(location)
+    image_tag "http://maps.google.com/maps/api/staticmap?size=450x300&sensor=false&zoom=16&markers=#{location.latitude}%2C#{location.longitude}", :class => "img-polaroid"
+  end
+  
+  def linked_map_image(location)
+    link_to_google_maps(location, map_image(location))
+  end
+  
 end
