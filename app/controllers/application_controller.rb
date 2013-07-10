8dc65273a73cca5fff1e0cc4ad52e55b62282653
Ben Teitelbaum <ben@teitelbaum.us>
2013-07-10 14:26:09 -0700

    use jQuery to update alerts if render_error is called from within an AJAX call

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   
   def render_error(options={})
     respond_to do |format|
+      format.js { 
+        flash[:alert] = options[:message]
+        render :partial => 'layouts/update_alerts'
+      }
       format.html { redirect_to root_path, :alert => options[:message] }
       format.json { render :json => { :message => options[:message] }, :status => options[:status] }
     end
