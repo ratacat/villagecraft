b3a9fa358a9d5bf426cceddd12622a0edbcfe564
Ben Teitelbaum <ben@teitelbaum.us>
2013-07-20 23:34:25 -0700

    auto_attend when signing back in using the "signup to attend" button and Facebook auth

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -4,16 +4,11 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+      auto_attend
     else
       if @user.save
         flash[:notice] = ["Welcome to Villagecraft"]
-        # FIXME: session[:auto_attend_event] cookie not sent; I think this has to do with the window.location= hack in _facebook.js.erb; 
-        # for now, rely on params[:auto_attend_event]; grrr.
-        if (m = /^\/events\/(?<event_uuid>\w+)$/.match(session[:previous_url])) and params[:auto_attend_event]
-          @event = Event.find_by_uuid(m[:event_uuid])
-          @user.attends << @event
-          flash[:notice] << 'You are signed up to attend this event'
-        end
+        auto_attend
         sign_in_and_redirect @user, :event => :authentication
       else
         flash[:alert] = "Could not complete your sign up via Facebook."
@@ -22,5 +17,17 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
       end
       
     end
+  end
+  
+  protected
+  def auto_attend
+    # FIXME: session[:auto_attend_event] cookie not sent; I think this has to do with the window.location= hack in _facebook.js.erb; 
+    # for now, rely on params[:auto_attend_event]; grrr.
+    if (m = /^\/events\/(?<event_uuid>\w+)$/.match(session[:previous_url])) and params[:auto_attend_event]
+      @event = Event.find_by_uuid(m[:event_uuid])
+      @user.attends << @event
+      flash[:notice] = [flash[:notice]] unless flash[:notice].is_a?(Array)
+      flash[:notice] << 'You are now signed up for this event'
+    end    
   end
 end
