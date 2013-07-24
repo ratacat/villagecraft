7da75a9086ca17658b31eac665d078dbc14adfba
Ben Teitelbaum <ben@teitelbaum.us>
2013-07-24 12:17:55 -0700

    blur_location? helper

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -6,4 +6,8 @@ module EventsHelper
       number_to_currency(event.price)      
     end
   end
+  
+  def blur_location?(event)
+    user_signed_in? and (current_user === event.host or current_user.attending_event?(event))
+  end
 end
