72f7dd4572fef5945b394a26e8c5ea44d87191f2
Ben Teitelbaum <ben@teitelbaum.us>
2013-07-29 17:48:43 -0700

    make the point using the SRID 4269 geometry

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -2,7 +2,7 @@ class Neighborhood < ActiveRecord::Base
 
   def Neighborhood.find_by_lat_lon(lat, lon)
     # FIXME: sanity-check lat, lon
-    Neighborhood.where("ST_Within(ST_MakePoint(#{lon},#{lat}), geom)=true").first
+    Neighborhood.where("ST_Within(ST_SetSRID(ST_MakePoint(#{lon},#{lat}), 4269), geom)=true").first
   end
   
 end
