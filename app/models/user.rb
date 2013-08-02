f4c296a1e9a53cb3ce4df2e34e4c734e40338ef4
Ben Teitelbaum <ben@teitelbaum.us>
2013-08-02 16:58:37 -0700

    whitespace

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   acts_as_paranoid
-  
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
@@ -23,16 +23,16 @@ class User < ActiveRecord::Base
     end
   end
   has_many :vclasses, :class_name => 'Vclass', :foreign_key => :admin_id
-           
+
   has_many :reviews, :foreign_key => :author_id, :dependent => :destroy
   belongs_to :location
   has_many :images, :dependent => :destroy
   belongs_to :profile_image, :class_name => 'Image'
-  
+
   normalize_attributes :first_name, :last_name, :email, :address
-  
+
   before_validation :find_or_create_location_from_address
-  
+
   validates :email, :presence => true
   validates :first_name, :presence => true
   validates :last_name, :presence => true
@@ -40,11 +40,11 @@ class User < ActiveRecord::Base
   validates :state, :presence => true
   validates :location, :presence => true
   validates_associated :location
-  
+
   def name
     "#{self.first_name} #{self.last_name}"
   end
-  
+
   def profile_img_src(size = :medium)
     if self.profile_image.blank?
       "/assets/homunculus.png"
@@ -52,13 +52,13 @@ class User < ActiveRecord::Base
       self.profile_image.img.url(size)
     end
   end
-  
+
   def profile_image=(f)
     i = Image.create!(:img => f, :user => self)
     self.profile_image_id = i.id
-  end  
-  
-  # straightforward rate of attendance 
+  end
+
+  # straightforward rate of attendance
   # TODO: compute a "velocity" 0-100 that weights recent attendance more highly
   def velocity
     if self.attends.completed.count > 0
@@ -75,18 +75,18 @@ class User < ActiveRecord::Base
   def state
     @state || self.location.try(:state_code)
   end
-  
+
   def distance_to(o)
     l = o.is_a?(Location) ? o : o.location
     Geocoder::Calculations.distance_between(self.location, l).round(2)
   end
-  
+
   # FIXME: stub
   def rating_as_host
     generator = Random.new # You need to instance it
     generator.rand(1.0..10.0).round(1)
   end
-  
+
   def attending_event?(e)
     self.attends.where('"attendances"."event_id"=?', e.id).exists?
   end
@@ -94,16 +94,16 @@ class User < ActiveRecord::Base
   def confirmed_attend_at_event?(e)
     self.attends.confirmed.where('"attendances"."event_id"=?', e.id).exists?
   end
-  
+
   def User.find_for_facebook_oauth(auth, signed_in_resource=nil)
     unless user = User.where(:auth_provider => auth.provider, :auth_provider_uid => auth.uid).first
       fb_profile_img_uri = URI.parse(auth.info.image)
       fb_profile_img_uri.query = "type=large"
       random_pwd = Devise.friendly_token[0,20]
-      
+
       location = Location.new_from_address(auth.info.location)
-      
-      user = User.find_by_email(auth.info.email) || 
+
+      user = User.find_by_email(auth.info.email) ||
              User.new(:email => auth.info.email,
                       :first_name => auth.info.first_name,
                       :last_name => auth.info.last_name,
@@ -133,17 +133,17 @@ class User < ActiveRecord::Base
       end
     else
       super
-    end    
+    end
   end
-  
+
   # FIXME: add admin attribute and remove this
   def admin?
     false
   end
-  
+
   protected
   def find_or_create_location_from_address
     self.location ||= Location.find_or_create_by_city_and_state_code(:city => self.city, :state_code => self.state) unless self.city.blank? or self.state.blank?
   end
-  
+
 end
