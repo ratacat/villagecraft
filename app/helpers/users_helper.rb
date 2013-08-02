772613e8d2961862e48a846b117a5dec8a11451b
Ben Teitelbaum <ben@teitelbaum.us>
2013-08-02 00:20:39 -0700

    format % display of velocity

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,4 +4,8 @@ module UsersHelper
     name = (user === current_user) ? 'you' : user.name
     link_to name, user
   end
+  
+  def formatted_velocity(user)
+    number_to_percentage(user.velocity * 100, precision: 0)
+  end
 end
