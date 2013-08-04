e2d67e439167afac8f6a45ec9c3f05fecdc18a46
Ben Teitelbaum <ben@teitelbaum.us>
2013-08-04 13:26:59 -0700

    basic activity feed at bottom of Event#show view

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -10,4 +10,34 @@ module EventsHelper
   def blur_event_location?(event)
     not (user_signed_in? and (current_user === event.host or current_user.attending_event?(event)))
   end
+  
+  def event_action(activity, options={})
+    event = activity.trackable
+    html = []
+    html << link_to(event.host.name, event.host) if event.host
+    case activity.key
+    when 'event.time_changed'
+      html << 'changed the time'
+    when 'event.create'
+      html << 'created the event'
+    else
+      ''
+    end
+    if options[:link_to_event]
+      case activity.key
+      when 'event.time_changed'
+        html << 'of'
+      else
+        ''
+      end
+      html << link_to(event.title, event)
+    end
+    case activity.key
+    when 'event.time_changed'
+      html << "to #{activity.parameters[:new_time]}"
+    else
+      ''
+    end
+    html.join(' ').html_safe
+  end
 end
