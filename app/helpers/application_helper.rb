820f5aa82852aae2be4afc13dbbe08f9fc3306b2
Ben Teitelbaum <ben@teitelbaum.us>
2013-08-04 21:23:17 -0700

    time_ago helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,8 @@ module ApplicationHelper
       plurals[verb.to_sym] ? plurals[verb.to_sym] : (verb + 's')
     end
   end
+  
+  def time_ago(time)
+    content_tag(:span, distance_of_time_in_words(time, Time.now) + ' ago', :'data-livestamp' => time, :class => 'muted')
+  end
 end
