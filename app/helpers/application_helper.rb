eb311a17818cd8888670cc131d0353a95a5869b4
Ben Teitelbaum <ben@teitelbaum.us>
2013-08-04 21:01:38 -0700

    conjugator helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def conjugate(verb, options={})
+    plurals = {}
+    if options[:second_person]
+      verb
+    else
+      plurals[verb.to_sym] ? plurals[verb.to_sym] : (verb + 's')
+    end
+  end
 end
