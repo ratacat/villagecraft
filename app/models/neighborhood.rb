340707e9105b3effd408658bd99b871815b81fda
Ben Teitelbaum <ben@teitelbaum.us>
2013-08-08 18:56:39 -0700

    add facility to reverse geocode from 'hood polygon (NB: centroid might not be in the same city)

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -1,4 +1,10 @@
 class Neighborhood < ActiveRecord::Base
+  reverse_geocoded_by :latitude, :longitude do |obj, results|
+    if geo = results.first
+      obj.city = geo.city
+      obj.state = geo.state_code
+    end
+  end
 
   def as_geo_json
     @hood = Neighborhood.select('*, ST_AsGeoJSON(geom) as json').where(:id => self.id).first
@@ -9,10 +15,25 @@ class Neighborhood < ActiveRecord::Base
     @hood = Neighborhood.select('*, ST_AsKML(geom) as kml').where(:id => self.id).first
     @hood.try(:kml)
   end
+  
+  def point_in_hood
+    @hood = Neighborhood.select('*, ST_AsText(ST_PointOnSurface(geom)) as pih').where(:id => self.id).first
+    @hood.pih.split(/[\(\) ]/)[1,2]
+  end
+  
+  def latitude
+    self.point_in_hood.last.to_f
+  end
+
+  def longitude
+    self.point_in_hood.first.to_f
+  end
 
   def Neighborhood.find_by_lat_lon(lat, lon)
     # FIXME: sanity-check lat, lon
     Neighborhood.where("ST_Within(ST_SetSRID(ST_MakePoint(#{lon},#{lat}), 4269), geom)=true").first
   end
+  
+  
   
 end
