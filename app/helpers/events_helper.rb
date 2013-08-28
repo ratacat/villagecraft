e8dfd645b40f9d0d4a0ba37bce75433c6443e30b
Ben Teitelbaum <ben@teitelbaum.us>
2013-08-28 01:33:06 -0700

    new helper: event_size; optionally show interest in attends_status

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -11,13 +11,24 @@ module EventsHelper
     not (user_signed_in? and (current_user === event.host or current_user.attending_event?(event)))
   end
   
-  def attends_status(event)
+  def attends_status(event, options={})
+    defaults = {
+      :show_interest => false
+    }
+    options.reverse_merge!(defaults)
+    
     attends = event.attendances.with_state(:attending).count
-    applied = event.attendances.with_state(:applied).count
+    applied = event.attendances.with_state(:interested).count
 
-    html = "#{pluralize(attends, 'person')} attending"
-    html += " (#{applied} interested)" if applied > 0
+    html = "#{pluralize(attends, 'attendee')}"
+    if options[:show_interest]
+      html += " (#{applied} interested)" if applied > 0      
+    end
     html
+  end
+
+  def event_size(event)
+    "#{event.min_attendees}-#{event.max_attendees} attendees"
   end
   
   def slots_status(event)
@@ -26,7 +37,7 @@ module EventsHelper
     max = event.max_attendees
 
     if attends < min
-      "minimum (#{min}) not met"
+      "need #{min - attends} more"
     else
       "#{(attends < max) ? pluralize(max - attends, 'slot') : 'no slots'} left"
     end
