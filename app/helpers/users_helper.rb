e49c942d689e5d343ff939eaf4820cfdce079ee1
Ben Teitelbaum <ben@teitelbaum.us>
2013-10-03 00:05:03 -0700

    respect :thumb_size and :linked options

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -15,7 +15,11 @@ module UsersHelper
   end
   
   def contextualized_user_link(user, options={})
-    link_to contextualized_user_name(user, options), user
+    if options[:linked]
+      link_to contextualized_user_name(user, options), user
+    else
+      contextualized_user_name(user, options)
+    end
   end
   
   def formatted_velocity(user)
@@ -38,12 +42,14 @@ module UsersHelper
   def rich_user_thumbnail(user, options={})
     defaults = {
       :thumbnail_classes => 'thumbnail right-caption clearfix',
-      :thumb_options => {:size => :thumb, :linked => true},
-      :user_link_options => {}
+      :user_link_options => {},
+      :linked => true,
+      :thumb_size => :thumb
     }
     options.reverse_merge!(defaults)
+    options[:user_link_options].reverse_merge!({:linked => options[:linked]})
     content_tag(:div, :class => options[:thumbnail_classes]) do
-      user_thumb(user, options[:thumb_options]) +
+      user_thumb(user, {:size => options[:thumb_size], :linked => options[:linked]}) +
       contextualized_user_link(user, options[:user_link_options])
     end
   end
