c972747ae8b78a9fc92e78b361f32494d3a6f212
Ben Teitelbaum <ben@teitelbaum.us>
2013-10-04 14:58:00 -0700

    client-side phone number formatting magic

---
+++ app/assets/javascripts/phone.js
@@ -0,0 +1,18 @@
+$.fn.format_phone_number = function() {
+  var country = 'us';
+  return this.each(function() {
+    var cur_val = $(this).val();
+    cur_val = cur_val.replace(/^\+1/,'');
+    $(this).val(formatLocal(country, cur_val));
+  });
+};
+
+$('#user_phone').format_phone_number();
+
+$('#user_phone').keypress(function(e) {
+  $(this).format_phone_number();
+});
+
+$('#user_phone').blur(function(e) {
+  $(this).format_phone_number();
+});
