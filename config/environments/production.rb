8d221e132aaacf80078f53d172bf2227e6090e62
Ben Teitelbaum <ben@teitelbaum.us>
2013-10-16 14:10:38 -0700

    use enable_starttls_auto option

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,10 +55,10 @@ Villagecraft::Application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :tls => :true,
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
+    :enable_starttls_auto => true,
     :user_name => 'Jared Smith',
     :password => 'slack3r101'
   }
