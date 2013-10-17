a1fa5d644a5b5df3699347909525157240028f31
Ben Teitelbaum <ben@teitelbaum.us>
2013-10-16 17:18:23 -0700

    redirect to root_path after create or update

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -77,7 +77,7 @@ class EventsController < ApplicationController
     respond_to do |format|
       if @event.save
         @event.create_activity :create, owner: current_user
-        format.html { redirect_to @event, notice: 'Event was successfully created.' }
+        format.html { redirect_to root_path, notice: 'Event was successfully created.' }
         format.json { render json: @event, status: :created, location: @event }
       else
         collate_when_errors
@@ -106,7 +106,7 @@ class EventsController < ApplicationController
                                  owner: current_user, 
                                  parameters: {:new_venue_id => @event.venue_id}          
         end
-        format.html { redirect_to events_path, notice: 'Event was successfully updated.' }
+        format.html { redirect_to root_path, notice: 'Event was successfully updated.' }
         format.json { head :no_content }
       else
         collate_when_errors
