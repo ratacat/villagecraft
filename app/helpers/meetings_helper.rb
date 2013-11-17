73564945163a6e7cdc52b63ac91ed1ce39273ba9
Ben Teitelbaum <ben@teitelbaum.us>
2013-11-17 11:02:40 -0800

    :wrapper_tag and :wrapper_options options for use when including in table

--- app/helpers/meetings_helper.rb
+++ app/helpers/meetings_helper.rb
@@ -4,21 +4,30 @@ module MeetingsHelper
       :short_date => false,
       :show_livestamp => false,
       :no_tz => false,
-      :tag => :span
+      :wrapper_tag => nil,
+      :wrapper_options => {}
     }
     options.reverse_merge!(defaults)
     options[:date_format] = options[:short_date] ? "%A %b" : "%A %B"
 
-    html = content_tag(options[:tag], meeting.localized_start_time.strftime(options[:date_format] + " #{meeting.start_time.day.ordinalize}"))
-    html += content_tag(options[:tag], "#{l meeting.localized_start_time, format: :short_time} - #{l meeting.localized_end_time, format: :short_time}")
-
+    html = []
+    html << content_tag(:span, meeting.localized_start_time.strftime(options[:date_format] + " #{meeting.start_time.day.ordinalize}"), :class => 'date')
+    html << content_tag(:span, "#{l meeting.localized_start_time, format: :short_time} - #{l meeting.localized_end_time, format: :short_time}", 
+                        :'data-start_time_date' => l(meeting.localized_start_time, format: :date_picker_date_format).strip,
+                        :'data-start_time_time' => l(meeting.localized_start_time, format: :time_picker_time_format).strip,
+                        :'data-end_time_date' => l(meeting.localized_end_time, format: :date_picker_date_format).strip,
+                        :'data-end_time_time' => l(meeting.localized_end_time, format: :time_picker_time_format).strip,
+                        :class => 'time_range')
     if options[:show_livestamp]
-      html += content_tag(options[:tag], '', :class => 'muted', :'data-livestamp' => meeting.start_time)
+      html << content_tag(:span, '', :class => 'muted', :'data-livestamp' => meeting.start_time)
     end
     unless options[:no_tz]
-      content_tag(options[:tag], friendly_time_zone_name(meeting.time_zone), :class => 'time_zone')
+      html << content_tag(:span, friendly_time_zone_name(meeting.time_zone), :class => 'time_zone')
     end
-    html.html_safe
+    if options[:wrapper_tag]
+      html.map! {|e| content_tag(options[:wrapper_tag], e, options[:wrapper_options]) }
+    end
+    html.join('').html_safe
   end
   
 end
