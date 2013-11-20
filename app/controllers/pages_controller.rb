793ffff5df854762496b24d7a3333b4e9e6c6f71
Ben Teitelbaum <ben@teitelbaum.us>
2013-11-19 17:18:57 -0800

    show unique workshops, using new scopes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def home
-    @events = Event.future.ordered_by_earliest_start_time
+    # FIXME: this isn't cheap; maybe cache it or move more of it into a fancy SQL query
+    @events = Workshop.future.ordered_by_earliest_meeting_start_time.map(&:next_rerun)
   end
 
   def about
