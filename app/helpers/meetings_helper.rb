a880718d08b3cd1ecc32bffd38ef18e134ebc9ae
Ben Teitelbaum <ben@teitelbaum.us>
2013-11-20 23:22:34 -0800

    for huge intervals switch to "(ending 5 days later)"

--- app/helpers/meetings_helper.rb
+++ app/helpers/meetings_helper.rb
@@ -14,8 +14,12 @@ module MeetingsHelper
     html = []
     html << content_tag(:span, meeting.localized_start_time.strftime(options[:date_format] + " #{meeting.localized_start_time.day.ordinalize}"), :class => 'date')
     html << options[:spacer] if options[:spacer]
-    time_interval = "#{l meeting.localized_start_time, format: :short_time} - #{l meeting.localized_end_time, format: :short_time}"
-    time_interval += " (+#{distance_of_time_in_words(meeting.start_time, meeting.end_time)})" if meeting.end_time - meeting.start_time >= 1.day
+    if meeting.end_time - meeting.start_time >= 2.days
+      time_interval = "#{l meeting.localized_start_time, format: :short_time} (ending #{distance_of_time_in_words(meeting.start_time, meeting.end_time)} later)"
+    else
+      time_interval = "#{l meeting.localized_start_time, format: :short_time} - #{l meeting.localized_end_time, format: :short_time}"
+      time_interval += " (+#{distance_of_time_in_words(meeting.start_time, meeting.end_time)})" if meeting.end_time - meeting.start_time >= 1.day
+    end
     html << content_tag(:span, time_interval, 
                         :'data-start_time_date' => l(meeting.localized_start_time, format: :date_picker_date_format).strip,
                         :'data-start_time_time' => l(meeting.localized_start_time, format: :time_picker_time_format).strip,
