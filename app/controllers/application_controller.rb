09f017981c0535feb8973fcebd75763b2da6b7c6
Ben Teitelbaum <ben@teitelbaum.us>
2013-11-21 17:30:44 -0800

    return AJAX errors the way x-editable wants them

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,10 @@ class ApplicationController < ActionController::Base
   end
   
   rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_url, :alert => exception.message
+    respond_to do |format|
+      format.html { redirect_to root_url, :alert => exception.message }
+      format.json { render :inline => exception.message, :status => :forbidden }
+    end
   end
   
   protected
