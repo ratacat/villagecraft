c5d2047d822f63c5165d0cc05833bd9f822c9538
Ben Teitelbaum <ben@teitelbaum.us>
2013-12-02 22:11:30 -0800

    massage JSONified Rails validation errors

--- app/assets/javascripts/x-editable-config.js
+++ app/assets/javascripts/x-editable-config.js
@@ -1,4 +1,10 @@
 $.fn.editable.defaults.ajaxOptions = {type: 'PUT'};
+$.fn.editable.defaults.error = 
+  function(response, newValue) { 
+    var field_name = $(this).data('name'),
+        error_msgs = response.responseJSON[field_name];
+    return error_msgs.join("; ");
+  };
 
 var editable_currency_display = 
   function(value, response) { 
