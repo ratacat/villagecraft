dab328651980fe17963e8338f0985c384e1a73d5
Ben Teitelbaum <ben@teitelbaum.us>
2013-12-23 15:22:51 -0800

    don't allow email address to be changed without enterring password

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,31 +9,6 @@ class RegistrationsController < Devise::RegistrationsController
     super
   end
 
-  def update
-    @user = User.find(current_user.id)
-
-    password_present = params[:user][:password].present?
-    
-    successfully_updated = if @user.has_set_password
-      @user.update_with_password(params[:user])
-    else
-      params[:user].delete(:current_password)
-      @user.update_attributes(params[:user])
-    end
-
-    if successfully_updated
-      if password_present
-        @user.update_attribute(:has_set_password, true)
-      end
-      set_flash_message :notice, :updated
-      # Sign in the user bypassing validation in case his password changed
-      sign_in @user, :bypass => true
-      redirect_to after_update_path_for(@user)
-    else
-      render "edit"
-    end
-  end
-
   protected
   def check_for_auto_attend
     if resource.persisted? # user is created successfuly
