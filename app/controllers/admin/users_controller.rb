e8ee5ce79d52a8f58b59afe34710a31bfca589e3
Ben Teitelbaum <ben@teitelbaum.us>
2013-12-23 19:25:04 -0800

    save not save! (that was there for testing)

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -16,7 +16,7 @@ class Admin::UsersController < UsersController
   def create
     @user.password ||= Devise.friendly_token[0,20]
     respond_to do |format|
-      if @user.save!
+      if @user.save
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render json: @user, status: :created, location: @user }
       else
