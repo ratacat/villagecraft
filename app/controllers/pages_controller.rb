071bcd4d6d6bd367ae612f2c55117e3ceffba08a
Ben Teitelbaum <ben@teitelbaum.us>
2013-12-23 22:00:45 -0800

    this was a tricky bug introduced by paranoia
    the join was picking up deleted meeting records

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,11 @@
 class PagesController < ApplicationController
   def home
     # FIXME: this isn't cheap; maybe cache it or move more of it into a fancy SQL query
-    @workshops_with_upcoming_or_ongoing_reruns = Workshop.joins(:first_meetings).where(%Q(#{Meeting.quoted_table_column(:end_time)} > ?), Time.now).order(%Q(#{Meeting.quoted_table_column(:start_time)})).to_a.uniq
+    @workshops_with_upcoming_or_ongoing_reruns = 
+      Workshop.joins(:first_meetings).
+      where(%Q(#{Meeting.quoted_table_column(:end_time)} > ?), Time.now).
+      where(%Q(#{Meeting.quoted_table_column(:deleted_at)} IS NULL)).
+      order(%Q(#{Meeting.quoted_table_column(:start_time)})).to_a.uniq
     @workshops = (@workshops_with_upcoming_or_ongoing_reruns + Workshop.where("id NOT IN (?)", @workshops_with_upcoming_or_ongoing_reruns))
   end
 
