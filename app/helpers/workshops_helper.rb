a6450352973e1527afea9ec90ed3eaec52fcbb24
Ben Teitelbaum <ben@teitelbaum.us>
2013-12-25 02:41:44 -0800

    make external link annotation a superscript (looks better)

--- app/helpers/workshops_helper.rb
+++ app/helpers/workshops_helper.rb
@@ -2,7 +2,7 @@ module WorkshopsHelper
   def annotated_workshop_title(workshop, options={})
     html = workshop.title
     if workshop.external?
-      html += " " + content_tag(:i, '', :class => 'icon-external-link')      
+      html += " " + content_tag(:sup, content_tag(:i, '', :class => 'icon-external-link'))
     end
     html.html_safe
   end
