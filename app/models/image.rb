cdce4026f5a628a14f9f9ecb1051c8b980fb60e8
Ben Teitelbaum <ben@teitelbaum.us>
2014-01-07 14:26:58 -0800

    new image size: large_orig_thumb_size

--- app/models/image.rb
+++ app/models/image.rb
@@ -7,6 +7,7 @@ class Image < ActiveRecord::Base
   has_attached_file :img, 
                     :styles => {:larger => "400x400>",  # original aspect ratio
                                 :xlarge => "270x270#",  # this is Bootstrap span3's width
+                                :large_orig => "200x200>",
                                 :large => "200x200#", 
                                 :medium => "100x100#", 
                                 :small => "50x50#", 
