78b2c0b55eda22dcbb996b60c070cf34adbb5d92
Ben Teitelbaum <ben@teitelbaum.us>
2014-01-16 17:03:34 -0800

    don't show parens for blank external URLs

--- app/helpers/workshops_helper.rb
+++ app/helpers/workshops_helper.rb
@@ -9,7 +9,7 @@ module WorkshopsHelper
     html = workshop.title
     html = link_to(html, options[:linked_to], :title => "#{'external workshop' if workshop.external?}") if options[:linked_to]
     
-    if workshop.external? and workshop.external_url
+    if workshop.external? and not workshop.external_url.blank?
       html += content_tag(:span, " (#{domain_suffix(workshop.external_url)})", :class => 'domain_annotation')
     end
     html.html_safe
