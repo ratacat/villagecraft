901bc2250de158a378c8d8b45904c825c99878e7
Ben Teitelbaum <ben@teitelbaum.us>
2014-01-16 16:55:05 -0800

    annotate workshop titles with gray domain name rather than external icon

--- app/helpers/workshops_helper.rb
+++ app/helpers/workshops_helper.rb
@@ -1,8 +1,16 @@
 module WorkshopsHelper
   def annotated_workshop_title(workshop, options={})
+    defaults = {
+      :linked_to => nil
+    }
+    options.reverse_merge!(defaults)
+    options[:linked_to] = workshop_path(workshop) if options[:linked_to] === true
+    
     html = workshop.title
-    if workshop.external?
-      html += " " + content_tag(:sup, content_tag(:i, '', :class => 'icon-external-link'))
+    html = link_to(html, options[:linked_to], :title => "#{'external workshop' if workshop.external?}") if options[:linked_to]
+    
+    if workshop.external? and workshop.external_url
+      html += content_tag(:span, " (#{domain_suffix(workshop.external_url)})", :class => 'domain_annotation')
     end
     html.html_safe
   end
