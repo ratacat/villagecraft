32283023e2396e0d984efbb648e3bd0de09433d4
Ben Teitelbaum <ben@teitelbaum.us>
2014-01-21 22:01:47 -0800

    move fancy windowing query to new Activity model

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -11,25 +11,7 @@ class Admin::DashboardController < ApplicationController
   
   protected
   def set_activities_n_counts(n=ACTIVITIES_PER_PAGE)
-    # returns an arrays of ids, like_ids, and a count; each record looks something like this:
-    # {"id"=>"451", "like_ids"=>"{456,451,452,453,454,455}", "like_id_count"=>"6"}
-    fancy_windowing_sql = %Q(
-      SELECT min(id) AS id, array_agg(id) AS like_ids, count(id) AS like_id_count 
-      FROM ( 
-        SELECT id, 
-               owner_id, 
-               created_at, 
-               row_number() OVER (ORDER BY id) - row_number() OVER (PARTITION BY trackable_id, trackable_type, owner_id, owner_type, key ORDER BY id) AS grp 
-        FROM activities 
-        ORDER BY id) t 
-      GROUP BY grp, owner_id 
-      ORDER BY max(id) 
-      DESC LIMIT #{n};
-    )    
-    records_array = ActiveRecord::Base.connection.execute(fancy_windowing_sql)
-    ids = records_array.map {|e| e["id"]}
-    counts = records_array.map {|e| e["like_id_count"].to_i}
-    @activities_n_counts = [PublicActivity::Activity.where(:id => ids).order(:id).reverse_order, counts].transpose    
+    @activities_n_counts = Activity.activities_n_counts(n)
   end
   
   def dash_auth!(action, subject)
