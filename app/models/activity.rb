9d82b61e89075035c833e9c0229351f6be7c21bc
Ben Teitelbaum <ben@teitelbaum.us>
2014-01-22 22:07:22 -0800

    this should be faster

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -16,10 +16,17 @@ class Activity < PublicActivity::Activity
       DESC LIMIT #{n};
     )    
     records_array = ActiveRecord::Base.connection.execute(fancy_windowing_sql)
-    ids = records_array.map {|e| e["id"]}
-    counts = records_array.map {|e| e["like_id_count"].to_i}
-    like_ids = records_array.map {|e| e["like_ids"]}
-    [PublicActivity::Activity.where(:id => ids).order(:id).reverse_order, counts, like_ids].transpose    
+    ids = []
+    counts = []
+    like_ids = []
+    records_array.each do |e|
+      ids << e['id']
+      counts << e["like_id_count"].to_i
+      like_ids << e["like_ids"]
+    end
+    PublicActivity::Activity.where(:id => ids).order(:id).reverse_order.each_with_index.map do |a, i|
+      {:activity => a, :count => counts[i], :like_ids => like_ids[i]}
+    end
   end
   
 end
