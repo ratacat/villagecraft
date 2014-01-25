7141103a6006f81a175060e8f2df71e8f2b55790
Ben Teitelbaum <ben@teitelbaum.us>
2014-01-24 17:51:52 -0800

    respect some limits (to allow debugging on live site)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,13 @@
 class PagesController < ApplicationController
   def home
+    l1 = params[:l1] || 1000
+    l2 = params[:l2] || 1000
     # FIXME: this isn't cheap; maybe cache it or move more of it into a fancy SQL query
     @workshops_with_upcoming_or_ongoing_reruns = 
       Workshop.joins(:first_meetings).
       where(%Q(#{Meeting.quoted_table_column(:end_time)} > ?), Time.now).
-      order(%Q(#{Meeting.quoted_table_column(:start_time)})).to_a.uniq
-    @workshops = (@workshops_with_upcoming_or_ongoing_reruns + Workshop.where("id NOT IN (?)", @workshops_with_upcoming_or_ongoing_reruns))
+      order(%Q(#{Meeting.quoted_table_column(:start_time)})).limit(l1).to_a.uniq
+    @workshops = (@workshops_with_upcoming_or_ongoing_reruns + Workshop.where("id NOT IN (?)", @workshops_with_upcoming_or_ongoing_reruns).limit(l2))
   end
 
   def about
