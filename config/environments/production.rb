ac1db45a861756b69dde5e7f5c1502bafce1a889
Ben Teitelbaum <ben@teitelbaum.us>
2014-01-31 18:10:34 -0800

    log queries that take too long

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Villagecraft::Application.configure do
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
-  # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  config.active_record.auto_explain_threshold_in_seconds = 0.5
   
   config.facebook_app_id = "215395121943347"
   config.facebook_app_secret = "82d5e3ab69b26620339c224601c6bac5"
