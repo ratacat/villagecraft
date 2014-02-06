8278bfbaa4330c011adba3e2124aa907a567412f
Ben Teitelbaum <ben@teitelbaum.us>
2014-02-06 00:42:21 -0800

    sort by distance view variant

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,29 @@
 class PagesController < ApplicationController
   def home
-    # FIXME: this isn't cheap; maybe cache it or move more of it into a fancy SQL query
-    @workshops_with_upcoming_or_ongoing_reruns = 
-      Workshop.joins(:first_meetings).
-      where(%Q(#{Meeting.quoted_table_column(:end_time)} > ?), Time.now).
-      order(%Q(#{Meeting.quoted_table_column(:start_time)})).to_a.uniq
-    @workshops = (@workshops_with_upcoming_or_ongoing_reruns + Workshop.where("id NOT IN (?)", @workshops_with_upcoming_or_ongoing_reruns))
+    if params[:sort]
+      session[:sort_order] = params[:sort]
+      redirect_to root_path
+    end
+    if session[:location_id]
+      @location = Location.find(session[:location_id])
+    elsif user_signed_in?
+      @location = current_user.location
+    else
+      @location = Location.find_or_create_by_address('Berkeley, CA')
+    end
+    q = Workshop.first_meeting_in_the_future
+    if session[:sort_order] == 'distance'
+      q = q.by_distance_from(@location)
+    end
+    q = q.order(%Q(#{Meeting.quoted_table_column(:start_time)}))
+    @workshops_with_upcoming_or_ongoing_reruns = q
+    @workshops = q.to_a.uniq
+    @other_workshops = 
+      if @workshops.blank?
+        Workshop.all
+      else      
+        Workshop.where("id NOT IN (?)", @workshops)
+      end
   end
 
   def about
