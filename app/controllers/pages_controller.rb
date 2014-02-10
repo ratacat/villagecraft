f0ffb63bfee0c031793757f4992ab31b35baf4fa
Ben Teitelbaum <ben@teitelbaum.us>
2014-02-10 13:37:31 -0800

    clean up the query build

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,19 +10,17 @@ class PagesController < ApplicationController
       @location = current_user.location
     end
     @location ||= Location.find_or_create_by_address('Berkeley, CA')
-    q = Workshop.first_meeting_in_the_future
+
+    @workshops = Workshop.first_meeting_in_the_future
     if session[:sort_order] == 'distance'
-      q = q.by_distance_from(@location)
+      @scheduled_workshops_with_venue_tbd = @workshops.where(:venue_id => nil)
+      @workshops = @workshops.by_distance_from(@location)
+      @other_workshops = Workshop.where(%Q(#{Workshop.quoted_table_column(:id)} NOT IN (#{@workshops.select(Workshop.quoted_table_column(:id)).to_sql})))
+        .where(%Q(#{Workshop.quoted_table_column(:id)} NOT IN (#{@scheduled_workshops_with_venue_tbd.select(Workshop.quoted_table_column(:id)).to_sql})))
+    else
+      @other_workshops = Workshop.where(%Q(#{Workshop.quoted_table_column(:id)} NOT IN (#{@workshops.select(Workshop.quoted_table_column(:id)).to_sql})))
     end
-    q = q.order(%Q(#{Meeting.quoted_table_column(:start_time)}))
-    @workshops_with_upcoming_or_ongoing_reruns = q
-    @workshops = q.to_a.uniq
-    @other_workshops = 
-      if @workshops.blank?
-        Workshop.all
-      else      
-        Workshop.where("id NOT IN (?)", @workshops)
-      end
+    @workshops = @workshops.order(%Q(#{Meeting.quoted_table_column(:start_time)}))
   end
 
   def about
