6b06e20de581ad9ee29ff7d504ee644eab137788
Ben Teitelbaum <ben@teitelbaum.us>
2014-02-11 23:39:58 -0800

    decorate the distances with Javascript so that cached workshop fragments may be used

--- app/assets/javascripts/util.js
+++ app/assets/javascripts/util.js
@@ -55,6 +55,26 @@ function auth_token() {
   return $("meta[name='csrf-token']").attr('content');
 }
 
+// compute the great circle distance ignoring ellipsoidal effects, which is fine over short distances; 
+// to get high-accuracy over large distances, use Vincenty’s formula (http://www.movable-type.co.uk/scripts/latlong-vincenty.html), 
+function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2-lat1);  // deg2rad below
+  var dLon = deg2rad(lon2-lon1); 
+  var a = 
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+    ; 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  var d = R * c; // Distance in km
+  return d;
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI/180)
+}
+
 jQuery(function($) {
   raty_ratings();
   popover_maps();
