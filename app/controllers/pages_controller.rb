fd6623ea26d02e3aaa4fe54b79f3ec4cd75ac3b6
Ben Teitelbaum <ben@teitelbaum.us>
2014-02-11 22:55:00 -0800

    fix the sort order for distance-mode

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -16,10 +16,11 @@ class PagesController < ApplicationController
       @scheduled_workshops_with_venue_tbd = @workshops.where(:venue_id => nil)
       @other_workshops = Workshop.where(%Q(#{Workshop.quoted_table_column(:id)} NOT IN (#{@workshops.select(Workshop.quoted_table_column(:id)).to_sql})))
         .where(%Q(#{Workshop.quoted_table_column(:id)} NOT IN (#{@scheduled_workshops_with_venue_tbd.select(Workshop.quoted_table_column(:id)).to_sql})))
+      @workshops = @workshops.by_distance_from(@location)
     else
       @other_workshops = Workshop.where(%Q(#{Workshop.quoted_table_column(:id)} NOT IN (#{@workshops.select(Workshop.quoted_table_column(:id)).to_sql})))
+      @workshops = @workshops.order(%Q(#{Meeting.quoted_table_column(:start_time)}))
     end
-    @workshops = @workshops.order(%Q(#{Meeting.quoted_table_column(:start_time)}))
   end
 
   def about
