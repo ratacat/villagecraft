08d089ea39868361c9034100f4f68d5b924f6e6e
Ben Teitelbaum <ben@teitelbaum.us>
2014-02-21 18:05:59 -0800

    find_apropos and validate that sender is authorized to send to it (e.g. all attendees)

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,41 @@
 class Message < ActiveRecord::Base
+  attr_accessor :_apropos_uuid
+
   belongs_to :from_user, :class_name => 'User'
   belongs_to :to_user, :class_name => 'User'
   belongs_to :apropos, :polymorphic => true
 
   has_uuid
-  acts_as_paranoid  
+  acts_as_paranoid
+  
+  before_validation :find_apropos, :on => :create
+  validates :from_user_id, presence: true
+  validate :can_send_to_apropos?
+  
+  protected
+  def find_apropos
+    unless self._apropos_uuid.blank?
+      case self.apropos_type
+      when 'Event'
+        self.apropos_id = Event.find_by_uuid(self._apropos_uuid).try(:id)
+      else
+        raise 'Unsupported message type'
+      end
+    end
+  end
+  
+  def can_send_to_apropos?
+    unless self.apropos.blank?
+      case self.apropos_type
+      when 'Event'
+        unless self.apropos.host === self.from_user
+          errors.add(:base, "Only the host of a rerun may message attendees")          
+        end
+      else
+        raise 'Unsupported apropos type'
+      end
+    end
+  end
+  
+  
 end
