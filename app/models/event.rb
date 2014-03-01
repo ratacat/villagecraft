52e61a16d969f743ce38a833e9e62b835af77197
Ben Teitelbaum <ben@teitelbaum.us>
2014-02-28 16:03:06 -0800

    if a rerun is given an external_url, but the parent workshop doesn't have one, mirror it

--- app/models/event.rb
+++ app/models/event.rb
@@ -39,7 +39,7 @@ class Event < ActiveRecord::Base
   after_initialize :generate_secret_if_missing
   normalize_attributes :title, :short_title, :description
   before_save :propogate_changes_to_dependant_meetings
-  after_save :touch_to_expire_cached_fragments
+  after_save :touch_to_expire_cached_fragments, :propogate_changes_to_parent_workshop
   
   validates :workshop, :presence => true
   validates :host_id, presence: true
@@ -228,6 +228,12 @@ class Event < ActiveRecord::Base
 
   def touch_to_expire_cached_fragments
     self.workshop.touch
+  end
+  
+  def propogate_changes_to_parent_workshop
+    if not self.external_url.blank? and self.workshop.external_url.blank?
+      self.workshop.update_attribute(:external_url, self.external_url)
+    end
   end
   
 end
