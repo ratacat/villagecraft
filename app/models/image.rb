a79123ebb07980228b29f1432de1533012d1e61a
Ben Teitelbaum <ben@teitelbaum.us>
2014-03-04 16:51:41 -0800

    remove old, dead, local img attachment (step four)

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,17 +3,7 @@ class Image < ActiveRecord::Base
   has_uuid
   acts_as_paranoid
   
-  attr_accessible :i, :img, :user
-  has_attached_file :img, 
-                    :styles => {:larger => "400x400>",  # original aspect ratio
-                                :xlarge => "270x270#",  # this is Bootstrap span3's width
-                                :large_orig => "200x200>",
-                                :large => "200x200#", 
-                                :medium => "100x100#", 
-                                :small => "50x50#", 
-                                :thumb => "32x32#" }, 
-                    :default_url => "/assets/homunculus_:style.png",
-                    :preserve_files => true
+  attr_accessible :i, :user
   
   AWS_ACCESS_KEY = 'AKIAII3AMFUMVNSGTVDA'
   AWS_SECRET_KEY = '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'
@@ -27,13 +17,4 @@ class Image < ActiveRecord::Base
                                 :small => "50x50#", 
                                 :thumb => "32x32#" }, 
                     :default_url => "/assets/homunculus_:style.png"
-
-  after_commit :keep_s3_in_sync
-  protected
-  def keep_s3_in_sync
-    if self.img.exists? and not self.i.exists?
-      self.i = Paperclip.io_adapters.for(self.img)
-      self.save
-    end
-  end                  
 end
