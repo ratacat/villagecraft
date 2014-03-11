024e55eee7824d4287c25a32002fb9e45828e520
Ben Teitelbaum <ben@teitelbaum.us>
2014-03-11 11:27:02 -0700

    pluralize class name

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -1,4 +1,4 @@
-module LocationHelper
+module LocationsHelper
   def link_to_google_maps(location, text=nil)
     link_to(text || location.address, "http://maps.google.com/?#{location.address.to_query('q')}")
   end
