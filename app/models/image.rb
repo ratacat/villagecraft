4107098b433c905924958c9141da766cf5eb8f02
Ben Teitelbaum <ben@teitelbaum.us>
2014-03-19 17:06:30 -0700

    new xlarge_orig size

--- app/models/image.rb
+++ app/models/image.rb
@@ -9,7 +9,8 @@ class Image < ActiveRecord::Base
   AWS_SECRET_KEY = 'yEdT2MOooLi4J4oxdepBoAnhk5pZ1BhHLCprvExm'
   has_attached_file :i, 
                     :storage => :s3, :bucket => 'villagecraft', :s3_headers => {'Expires' => 1.year.from_now.httpdate}, :s3_credentials => {:access_key_id => AWS_ACCESS_KEY, :secret_access_key => AWS_SECRET_KEY},
-                    :styles => {:larger => "400x400>",  # original aspect ratio
+                    :styles => {:xlarge_orig => "800x800>",
+                                :larger => "400x400>",  # original aspect ratio
                                 :col3 => "270x270#",  # this is Bootstrap col-md-3's width
                                 :large_orig => "200x200>",
                                 :large => "200x200#", 
