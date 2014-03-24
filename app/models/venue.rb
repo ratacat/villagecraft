92a951aebd28cc0c7e72cc2de120ef439c19293e
Ben Teitelbaum <ben@teitelbaum.us>
2014-03-24 15:06:40 -0700

    make venue location editable through venues form

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,6 +1,4 @@
 class Venue < ActiveRecord::Base
-  attr_accessible :name, :street, :city, :state_code, :address
-  attr_writer :street, :city, :state_code, :address
   has_uuid(:length => 8)
   acts_as_paranoid
 
@@ -8,41 +6,15 @@ class Venue < ActiveRecord::Base
   belongs_to :location
   has_many :meetings
   
-  before_validation :create_location
-  
-  validates :name, :presence => true
-  validates :owner, :presence => true
-
-  validates :street, :presence => true
-  validates :city, :presence => true
-  validates :state_code, :presence => true
-  
   validates :location, :presence => true
   validates_associated :location
-
-  def street
-    @street || self.location.try(:street)
-  end
-  
-  def city
-    @city || self.location.try(:city)
-  end
-
-  def state_code
-    @state_code || self.location.try(:state_code)
-  end
-  
-  def address
-    @address || self.location.try(:address)
-  end
   
   def public?
     false
   end
   
-  protected
-  def create_location
-    self.location = Location.create(:address => self.address, :street => self.street, :city => self.city, :state_code => self.state_code)
+  def location=(params)
+    self.create_location(params)
   end
   
 end
