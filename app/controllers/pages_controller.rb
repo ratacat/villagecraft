affeef08edb2f91f615eef82103ded08be851c33
Jarek Wozniak <jarwoz@gmail.com>
2014-05-12 11:24:50 +0200

    Event based home page /new-home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -25,6 +25,33 @@ class PagesController < ApplicationController
     @workshops = @workshops.to_a.uniq
   end
 
+
+  def home_events
+    if params[:sort]
+      session[:sort_order] = params[:sort]
+      redirect_to root_path
+    end
+    if user_signed_in?
+      @location = (current_user.last_sighting.try(:location) || current_user.location)
+    end
+    if session[:location_id]
+      @location ||= Location.find(session[:location_id])
+    end
+    @location ||= Location.find_or_create_by_address('Berkeley, CA')
+
+    @events = Event.first_meeting_in_the_future
+    if session[:sort_order] == 'distance'
+      @scheduled_events_with_venue_tbd = @events.where(:venue_id => nil)
+      @other_events = Event.where(%Q(#{Event.quoted_table_column(:id)} NOT IN (#{@events.select(Event.quoted_table_column(:id)).to_sql})))
+      .where(%Q(#{Event.quoted_table_column(:id)} NOT IN (#{@scheduled_events_with_venue_tbd.select(Event.quoted_table_column(:id)).to_sql}))).limit(15)  #limit added because it was too slow. Pagination needed
+      @events = @events.by_distance_from(@location)
+    else
+      @other_events = Event.where(%Q(#{Event.quoted_table_column(:id)} NOT IN (#{@events.select(Event.quoted_table_column(:id)).to_sql}))).limit(15) #limit added because it was too slow. Pagination needed
+      @events = @events.order(%Q(#{Meeting.quoted_table_column(:start_time)}))
+    end
+
+    @events = @events.to_a.uniq
+  end
   def about
   end
 end
