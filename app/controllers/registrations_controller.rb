037e2f41e8479520736240a892dcac67567f36b5
ratacat <neuralsplash@gmail.com>
2014-06-09 10:39:36 -0700

    mixpanel alias on registration

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,12 +9,15 @@ class RegistrationsController < Devise::RegistrationsController
     super
 
     user = User.find_by_email(params[:user][:email])
+    distinct_id = cookies[:mp_cd5f1afe1374c3c354a379627be6c27d_mixpanel].gsub(/"(.*)":\s"(.*)","(.*)":\s"(.*)","(.*)":\s"(.*)"/, '\2').gsub(/[\{\}]/, '')
 
     if user
+      @mixpanel.alias(user.email, distinct_id)
       @mixpanel.track(user.email, 'Registration', {
           'source' => "villagecraft",
           'modal' => params[:user][:modal]
       })
+
       @mixpanel.people.set(user.email, {'$name' => user.name, 'account_created_at' => DateTime.now}, request.remote_ip)
     end
   end
