858ae3c6645a3d060f61f051f520e1c789212d35
Sterling Radcliff <radcliff@riseup.net>
2014-08-23 20:31:39 -0700

    adds ability to collect fees from stripe charge

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -5,14 +5,20 @@ class ChargesController < ApplicationController
     event = Event.find(params[:event_id])
     token = params[:stripeToken]
     amount = (event.price.to_i) * 100
+    
+    unless (fee = amount / 10) > 100
+      fee = 100
+    end
 
     charge = Stripe::Charge.create(
       {
         :card        => token,
         :amount      => amount,
         :description => event.title,
-        :currency    => 'usd'
-      }
+        :currency    => 'usd',
+        :application_fee => fee
+      },
+      ACCESS_TOKEN # stripe token for host
     )
 
   rescue Stripe::CardError => e
