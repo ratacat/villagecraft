4421796c5fdfbec951b9423acb480e3946eca31c
mathur7 <mathur7@gmail.com>
2014-09-04 10:04:51 -0700

    changed controller methods to accommodate nesting of comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,29 +1,40 @@
 class CommentsController < ApplicationController
-  before_filter :load_event
+  before_filter :load_parent
 
   def index
-    @comments = @event.comments.all
-    @user = current_user
+    @comments = @parent.comments.all
   end
 
-  def create  
-    @comment = @event.comments.new comment_params
-    @comment.user_id = current_user.id
-    if @comment.save
-      redirect_to event_path(@event)
-    else
-      render 'new'
+  def new
+    @comment = @parent.comments.build
+  end
+ 
+  def create
+    @comment = @parent.comments.build(params[:comment])
+    @comment.user_id = current_user.id 
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to event_path(@parent), notice: 'Comment was successfully created.' }
+        format.json { render json: @comment, status: :created }
+        # format.js {render partial:"create"}
+      else
+        format.html { render action: "new" }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
     end
   end
     
-private
+protected
   
-  def comment_params
-    params.require(:comment).permit(:body)
-  end
+  # def comment_params
+  #   params.require(:comment).permit(:body, :commentable_id, :commentable_type)
+  # end
 
-  def load_event
-    @event = Event.find(params[:comment][:id])
+  def load_parent
+    # @event = Event.find(params[:event_id])  
+    @parent = Event.find_by_id(params[:comment][:id] ) if params[:comment][:id] 
+    @parent = Comment.find_by_id(params[:comment_id]) if params[:comment_id]
+    # redirect_to root_path unless defined?(@parent)
   end
 
 end
