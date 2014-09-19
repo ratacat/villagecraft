7f7799d4bf8735635198695323aba8c444adcaed
Sterling Radcliff <radcliff@riseup.net>
2014-09-18 20:42:59 -0700

    creates stripe customer at checkout; adds stripe customer token to user record

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -10,19 +10,36 @@ class ChargesController < ApplicationController
       fee = 100
     end
 
+    unless current_user.stripe_customer_id
+      binding.pry
+      customer = Stripe::Customer.create(
+        :card => token[:id],
+        :description => current_user.name
+      )
+      if customer
+        current_user.stripe_customer_id = customer.id
+        current_user.save
+      end
+    end
+
+    customer_id = current_user.stripe_customer_id
+
     Stripe::Charge.create(
       {
-        :card        => token,
+        :customer    => customer_id,
         :amount      => amount,
         :description => event.title,
         :currency    => 'usd',
         :application_fee => fee
       },
-      event.host.stripe_token # token from event host stripe connect
+      event.host.stripe_token # host token from event host stripe connect
     )
 
   rescue Stripe::CardError => e
     flash[:error] = e.message # pass key in view: `error`
+    render json: e
+    return
+  rescue => e
     render json: e
     return
   end
