c1b34f1805dc51709330e7af4bd9b1de1286c8c7
Sterling Radcliff <radcliff@riseup.net>
2014-09-27 15:03:48 -0700

    adds refund to chargesHelper

--- app/helpers/charges_helper.rb
+++ app/helpers/charges_helper.rb
@@ -1,2 +1,30 @@
 module ChargesHelper
+  def refund
+    if @event.rsvp && @event.price > 0
+      charge = Charge.where(event_id: @event).where(user_id: current_user).limit(1)[0]
+      stripe_charge = Stripe::Charge.retrieve(charge.stripe_charge) #grab charge record from stripe
+      
+      if refund = stripe_charge.refunds.create #issue stripe refund
+        charge.refunded = true #set column in db
+        if charge.save
+          return true
+        else
+          raise "refund unsuccessful"
+        end
+      else
+        raise "refund unsuccessful"
+      end
+
+    else
+      return true
+    end
+
+    rescue => e
+        puts
+        puts "*******"
+        puts "ERROR: " + e.message
+        puts "*******"
+        return false
+  end
 end
+
