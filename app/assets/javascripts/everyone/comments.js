9370a72f5c3c0d13886b79f0ed7cfb1f96cebf3b
Ben Teitelbaum <ben@teitelbaum.us>
2014-10-19 23:40:37 -0700

    oops, that was only there for testing #5

--- app/assets/javascripts/everyone/comments.js
+++ app/assets/javascripts/everyone/comments.js
@@ -1,9 +1,8 @@
-var form;
 $(document).ready(function(){
     $('.open-reply-form').on('click', function(e){
         e.preventDefault();
         $(this).hide();
-        form = $(this).parent().next('form');
+        var form = $(this).parent().next('form');
         form.show();
         form.find("input:not([type=hidden]):first").focus();
     });
