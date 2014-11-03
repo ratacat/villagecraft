fd5c50206e5626567afb22efec7af75cd4a8863c
antmachine <anthonysallows@gmail.com>
2014-11-03 12:34:58 -0800

    Trivial commit

--- app/helpers/charges_helper.rb
+++ app/helpers/charges_helper.rb
@@ -3,7 +3,7 @@ module ChargesHelper
     if event.rsvp && event.price > 0
       charge = Charge.where(event_id: event).where(user_id: attendee).limit(1)[0]
       stripe_charge = Stripe::Charge.retrieve(charge.stripe_charge) #grab charge record from stripe
-      
+
       if refund = stripe_charge.refunds.create #issue stripe refund
         charge.refunded = true #set column in db
         if charge.save
