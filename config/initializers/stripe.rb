1d95c8c6e61160fd7df2d4a3193a8436b568c666
antmachine <anthonysallows@gmail.com>
2014-11-06 22:48:23 -0800

    added quotes around stripe keys

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  :publishable_key => pk_live_4aOC2jj4S9mYQnFVfhUIPJc1,
-  :secret_key      => sk_live_4aOClJEMsLtrqAySFo8eGiNO
+  :publishable_key => 'pk_live_4aOC2jj4S9mYQnFVfhUIPJc1',
+  :secret_key      => 'sk_live_4aOClJEMsLtrqAySFo8eGiNO'
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
