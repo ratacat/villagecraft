7cfa1f730a3b0e0127f04a8de9e3549cdd291cd1
antmachine <anthonysallows@gmail.com>
2014-11-06 22:40:02 -0800

    Added live stripe keys to stripe.rb config file

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  :publishable_key => ENV['PUBLISHABLE_KEY'],
-  :secret_key      => ENV['SECRET_KEY']
+  :publishable_key => pk_live_4aOC2jj4S9mYQnFVfhUIPJc1,
+  :secret_key      => sk_live_4aOClJEMsLtrqAySFo8eGiNO
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
