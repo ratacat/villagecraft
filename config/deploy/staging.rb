1d53bd80c4b9b60057f5de4365d6260d85704eb3
antmachine <anthonysallows@gmail.com>
2014-12-04 12:22:58 -0800

    Testing exclusion of .conf in staging evnironment

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,2 +1,10 @@
 set :domain, 'test.villagecraft.org'
+# UNTESTED the following should exclude .conf files to avoid sending prodcution configurations to staging server
+set :copy_exclude, [".git/", "config/*.conf"]
 server 'test.villagecraft.org', :app, :web, :db, :primary => true
+
+
+# task :mirror_db_from_live_server do 
+# 	run 'ssh villagecraft@villagecraft.org "pg_dump -c villagecraft_production" | psql villagecraft_production'
+# end
+
